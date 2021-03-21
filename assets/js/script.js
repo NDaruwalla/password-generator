34d5da3c3136300df3349d3b3b599a3fadf84d85
Nicole Daruwalla <nicole.daruwalla@gmail.com>
2021-03-21 17:09:54 -0400

    added comments to readme, added code to array selection

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,18 +15,25 @@ generateBtn.addEventListener("click", writePassword);
 
 
 
-// Pseudocode 
 
+// these are the 'buckets' to hold the arrays.
+let availArray = [];
+let desiredArray = [];
+
+// Pseudocode 
+//Present prompts to determine criteria:
 // confirm Statements - TRUE or FALSE: 
 let confirmUpper = confirm ("Do you want to include uppercase characters?");
 let confirmLower = confirm ("Do you want to include lowercase characters?");
 let confirmNum = confirm ("Do you want to include numbers?");
 let confirmSpecial = confirm ("Do you want to include special characters?");
 //Validate that at least one array has been selected.
+let alertCharSet = ("You must select at least one character set.");
 
 // prompt statements
 //(prompt) Enter a number to represent the length of your password (must be at least 8 characters and no more than 128 characters)
 let promptLength = prompt ("Enter a number for the length for your password. Enter at least 8, but no more than 128.");
+let alertPickLength = ("You must select a number of at least 8, but no more than 128 for the length of your password.");
 
 
 
@@ -41,31 +48,39 @@ let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 let special = ["&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~", "!", "#", "$", "%"];
 //Question: should these be arrays or strings? Possibly need to try both ways.
 
-//If  statements to determine which arrays need to be used in password generation. If true, add (concatenate) array.
+//If  statements to determine which arrays need to be used in password generation. If true, add (concatenate) array. Add all of the arrays selected together. Alert if no character set is selected.
 if (confirmUpper) {
-
+  availArray = availArray.concat(uppercase)
 }
 
 
 if (confirmLower) {
-  
+  availArray = availArray.concat(lowercase)
 }
 
 
 if (confirmNum) {
-  
+  availArray = availArray.concat(numbers)
 }
 
 
 if (confirmSpecial) {
-  
-}
+  availArray = availArray.concat(special)
+  }
+
+// I'll need the following to generate the password from the array selection(s) gathered above:
+// use array.push method to add characters to the array  https://www.w3schools.com/jsref/jsref_push.asp
+// use Math.random to return a random number between 0 (inclusive),  and 1 (exclusive) note: it returns a number always lower than 1  https://www.w3schools.com/js/js_random.asp
+// times the random number by the pasword length selected. 
+// use Math.floor to round down. (The floor() method rounds a number DOWNWARDS to the nearest integer, and returns the result. https://www.w3schools.com/jsref/jsref_floor.asp
 
 
 
+
+// use array.join method to remove the commas from result  https://www.w3schools.com/jsref/jsref_join.asp
 
 //Click "generate password" button to begin password generation process
-//Present prompts to determine criteria:
+
 
 
 
@@ -80,8 +95,8 @@ Do you want to include numbers?
 Do you want to include special characters? 
 
 Repeat until at least one character set is selected. */
-let alertCharSet = ("You must select at least one character set.");
-let alertPickLength = ("You must select a number of at least 8, but no more than 128 for the length of your password.");
+
+
 
 
 
