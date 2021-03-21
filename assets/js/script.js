3abc7332b8e072e4eb9b288ddbe01f6a32e439aa
Nicole Daruwalla <nicole.daruwalla@gmail.com>
2021-03-21 12:21:28 -0400

    created variables and arrays for uppercase, lowercase, number, special characters; created variables for confirm statements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 // Assignment Code
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -7,38 +8,38 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword); 
 
-// Pseudocode
+
+
+
+// Pseudocode 
 
 //Character arrays needed for password generation:
 //uppercase letters
-let uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-
 //lowercase letters
-let lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-
 //numbers
-let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-
 //special characters
-let special = ["U+0021",	"U+0022",	"U+0023",	"U+0024",	"U+0025",	"U+0026",	"U+0027",	"U+0028",	"U+0029",	"U+002A",	"U+002B",	"U+002C",	"U+002D",	"U+002E",	"U+002F",	"U+003A",	"U+003B",	"U+003C",	"U+003D",	"U+003E",	"U+003F",	"U+0040",	"U+005B",	"U+005C",	"U+005D",	"U+005E",	"U+005F",	"U+0060",	"U+007B",	"U+007C",	"U+007D",	"U+007E"]
+let uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+let lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+let special = ["&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~", "!", "#", "$", "%"]
 
-//
-/*Click "generate password" button to begin password generation process
-Present prompts to determine criteria:
 
-(confirm)(TRUE or FALSE):
-Do you want to include uppercase characters? yes, no
-Do you want to include lowercase characters? yes, no
-Do you want to include numbers? yes, no
-Do you want to include special characters? yes, no
+//Click "generate password" button to begin password generation process
+//Present prompts to determine criteria:
 
-Validate that at least one array has been selected.
+//(confirm)(TRUE or FALSE): 
+let confirmUpper = (confirm) ("Do you wnat to include uppercase characters?");
+let confirmLower = (confirm) ("Do you want to include lowercase characters?");
+let confirmNum = (confirm) ("Do you wnat to include numbers?");
+let confirmSpecial = (confirm) ("Do you want to include special characters?");
+
+
+/*Validate that at least one array has been selected.
 
 If none was selected, 
 (alert)"You must select at least one character set." Then (confirm) character sets again:
@@ -61,8 +62,10 @@ Loop until number selected is selected is at least 8 characters and no more than
 
 Loop through each array selecting one random character from each array until the password length is reached.
 
-Next, take the password created and scramble/shuffle/randomize the order of the characters in the password. (Scrambling the characters is required because while characters are randomly selected from each array, due to the order of the array, the character set type (i.e., uppercase, lowercase, numbers,  special characters) will be predictable if they are not scrambled).
+Next, take the password created and scramble/shuffle/randomize the order of the characters in the password. 
+(Scrambling the characters is required because while characters are randomly selected from each array, due to the order of the array, the character set type (i.e., uppercase, lowercase, numbers,  special characters) will be predictable if they are not scrambled).
 
 Generate a random password from array that contains only selected character sets and in the length that was selected.
 
-Display the secure password in the text area on the page  */
+Display the secure password in the text area on the page 
+ */
